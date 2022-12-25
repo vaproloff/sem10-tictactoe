25ce8c5a96b69948915981b7c6d56916b95b42c6
Konstantin Pivnov <vaproloff@ya.ru>
2022-12-25 14:17:12 +0300

    modified game ui and logic

--- game_logic.py
+++ game_logic.py
@@ -1,6 +1,6 @@
 import random
 import time
-from main import draw_move
+from game_ui import *
 
 
 def generate_board():
@@ -19,40 +19,44 @@ def check_position(position, board):
         return board[position[0]][position[1]] == ' '
 
 
-def check_board_full(board):
+def check_board_full(screen, board):
     for i in range(len(board)):
         for j in board[i]:
             if j == ' ':
                 return False
-    return True
+    draw_status(screen, 'Ничья')
+    time.sleep(3)
+    pygame.quit()
+    exit()
 
 
-def check_win(board):
-    for i in board:
-        if i.count(True) == len(board) or i.count(False) == len(board):
-            return True
+def check_win(screen, board, is_player):
+    is_winner = False
+    for col in range(len(board)):
+        if board[col].count(True) == len(board) or board[col].count(False) == len(board):
+            is_winner = True
+            draw_win_line(screen, ((col + 1) * 100 - 50, 0), ((col + 1) * 100 - 50, 300))
+            break
     for i in range(len(board)):
-        col = [board[j][i] for j in range(len(board))]
-        if col.count(True) == len(board) or col.count(False) == len(board):
-            return True
+        row = [board[j][i] for j in range(len(board))]
+        if row.count(True) == len(board) or row.count(False) == len(board):
+            is_winner = True
+            draw_win_line(screen, (0, (i + 1) * 100 - 50), (300, (i + 1) * 100 - 50))
+            break
     diag1 = [board[i][i] for i in range(len(board))]
     if diag1.count(True) == len(board) or diag1.count(False) == len(board):
-        return True
+        is_winner = True
+        draw_win_line(screen, (50, 50), (250, 250))
     diag2 = [board[i][-i - 1] for i in range(len(board))]
     if diag2.count(True) == len(board) or diag2.count(False) == len(board):
-        return True
-    return False
+        is_winner = True
+        draw_win_line(screen, (250, 50), (50, 250))
 
-
-def player_turn(board):
-    position = [-1, -1]
-    while not check_position(position, board):
-        position = [int(input(f'Ваш ход. Введите номер строки (1-{len(board)}): ')) - 1,
-                    int(input(f'Введите номер столбца (1-{len(board)}): ')) - 1]
-        if not check_position(position, board):
-            print('Неверная позиция. Попробуйте снова')
-            time.sleep(0.5)
-    return position
+    if is_winner:
+        draw_status(screen, f'Выиграл {"игрок. Поздравляем!" if is_player else "бот"}')
+        time.sleep(3)
+        pygame.quit()
+        exit()
 
 
 def bot_turn(board):
@@ -63,19 +67,32 @@ def bot_turn(board):
     return position
 
 
+def make_turn(screen, board, position, is_player):
+    board[position[0]][position[1]] = is_player
+    draw_move(screen, position, is_player)
+    check_win(screen, board, is_player)
+    check_board_full(screen, board)
+    return not is_player
+
+
 def play_game(board):
+    pygame.init()
+    screen = init_board()
+
     is_player = True
-    while not check_board_full(board):
-        if is_player:
-            position = player_turn(board)
-        else:
-            position = bot_turn(board)
-        board[position[0]][position[1]] = is_player
-        draw_move(position, is_player)
-        if check_win(board):
-            print(f'Игра завершена. {"Вы выиграли. Поздравляем!" if is_player else "Выиграл бот:("}')
-            break
-        is_player = not is_player
-        time.sleep(0.5)
-    else:
-        print('Никто не выиграл')
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                position = click_board()
+                if check_position(position, board):
+                    is_player = make_turn(screen, board, position, is_player)
+            else:
+                if not is_player:
+                    draw_status(screen, 'Ход бота')
+                    position = bot_turn(board)
+                    is_player = make_turn(screen, board, position, is_player)
+                draw_status(screen, 'Ход игрока')
--- game_ui.py
+++ game_ui.py
@@ -1,42 +1,62 @@
 import pygame
 
-WIDTH = 300
-HEIGHT = 300
-
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Tic Tac Toe")
-clock = pygame.time.Clock()
-screen.fill((255, 255, 255))
-
-pygame.draw.line(screen, (0, 0, 0), (100, 0), (100, 300), 2)
-pygame.draw.line(screen, (0, 0, 0), (200, 0), (200, 300), 2)
-pygame.draw.line(screen, (0, 0, 0), (0, 100), (300, 100), 2)
-pygame.draw.line(screen, (0, 0, 0), (0, 200), (300, 200), 2)
+BOARD_WIDTH = 300
+BOARD_HEIGHT = 330
+CROSS_HALFWIDTH = 30
+CIRCLE_RADIUS = 30
+LINE_WIDTH = 2
+WINLINE_WIDTH = 5
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+COLOR_GREEN = (0, 255, 0)
 
 
-def draw_move(position, is_cross):
+def init_board():
+    screen = pygame.display.set_mode((BOARD_WIDTH, BOARD_HEIGHT))
+    pygame.display.set_caption("Tic Tac Toe")
+    screen.fill(COLOR_WHITE)
+    pygame.draw.line(screen, COLOR_BLACK, (100, 0), (100, 300), LINE_WIDTH)
+    pygame.draw.line(screen, COLOR_BLACK, (200, 0), (200, 300), LINE_WIDTH)
+    pygame.draw.line(screen, COLOR_BLACK, (0, 100), (300, 100), LINE_WIDTH)
+    pygame.draw.line(screen, COLOR_BLACK, (0, 200), (300, 200), LINE_WIDTH)
+    update_board()
+    return screen
+
+
+def update_board():
+    pygame.display.flip()
+
+
+def draw_move(screen, position, is_cross):
     center_x = position[0] * 100 + 50
     center_y = position[1] * 100 + 50
 
     if is_cross:
-        pygame.draw.line(screen, (0, 0, 0), (center_x - 20, center_y - 20),
-                         (center_x + 20, center_y + 20), 2)
+        pygame.draw.line(screen, COLOR_BLACK, (center_x - CROSS_HALFWIDTH, center_y - CROSS_HALFWIDTH),
+                         (center_x + CROSS_HALFWIDTH, center_y + CROSS_HALFWIDTH), LINE_WIDTH)
 
-        pygame.draw.line(screen, (0, 0, 0), (center_x + 20, center_y - 20),
-                         (center_x - 20, center_y + 20), 2)
+        pygame.draw.line(screen, COLOR_BLACK, (center_x + CROSS_HALFWIDTH, center_y - CROSS_HALFWIDTH),
+                         (center_x - CROSS_HALFWIDTH, center_y + CROSS_HALFWIDTH), LINE_WIDTH)
     else:
-        pygame.draw.circle(screen, (0, 0, 0), (center_x, center_y), 40, 2)
+        pygame.draw.circle(screen, COLOR_BLACK, (center_x, center_y), CIRCLE_RADIUS, LINE_WIDTH)
+
+    update_board()
 
 
-def get_position(mouse_x, mouse_y):
+def draw_win_line(screen, coord_1, coord_2):
+    pygame.draw.line(screen, COLOR_GREEN, coord_1, coord_2, WINLINE_WIDTH)
+    update_board()
+
+
+def click_board():
+    (mouse_x, mouse_y) = pygame.mouse.get_pos()
+
     if mouse_y < 100:
         row = 0
     elif mouse_y < 200:
         row = 1
     else:
         row = 2
-
     if mouse_x < 100:
         col = 0
     elif mouse_x < 200:
@@ -44,29 +64,12 @@ def get_position(mouse_x, mouse_y):
     else:
         col = 2
 
-    return [row, col]
+    return [col, row]
 
 
-def click_board():
-    (mouseX, mouseY) = pygame.mouse.get_pos()
-    position = get_position(mouseX, mouseY)
-    draw_move(position, True)
-
-
-def show_board(ttt, board):
-
-    # (re)draw the game board (board) on the screen (ttt)
-    # drawStatus(board)
-    ttt.blit (board, (0,0))
-    pygame.display.flip()
-
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        elif event.type is pygame.MOUSEBUTTONDOWN:
-            click_board()
-            show_board(ttt, board)
+def draw_status(screen, message):
+    font = pygame.font.SysFont('couriernew', 24)
+    text = font.render(message, True, COLOR_WHITE)
+    pygame.draw.rect(screen, COLOR_BLACK, (0, 300, 300, 330), 0)
+    screen.blit(text, (5, 300))
+    update_board()
